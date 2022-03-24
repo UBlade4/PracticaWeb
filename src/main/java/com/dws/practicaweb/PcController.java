e304a3997882aae9cb34b4bb85989b5e05e8e4e2
Alex <userblade4@gmail.com>
2022-03-24 11:57:12 +0100

    almacen

--- src/main/java/com/dws/practicaweb/PcController.java
+++ src/main/java/com/dws/practicaweb/PcController.java
@@ -1,7 +1,70 @@
 package com.dws.practicaweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.annotation.PostConstruct;
 
 @Controller
+@RequestMapping("/pcs")
 public class PcController {
+    @Autowired
+    private PcService servicePc;
+
+
+    @PostConstruct
+    public void init() {
+        servicePc.addPc(new Pc("12", "1080 GTX 4GB", "Intel I5 8va Gen"));
+        servicePc.addPc(new Pc("4", "126 MB", "Intel I4 5va Gen"));
+
+
+    }
+
+    @GetMapping("/newPc")
+    public String newPc(Model model, @RequestParam String ram, @RequestParam String graphicCard, @RequestParam String cpu){
+        Pc aux = new Pc(ram, graphicCard, cpu);
+        servicePc.addPc(aux);
+        return "pcCreated";
+    }
+
+    @GetMapping("/showPcs")
+    public String showPcs(Model model){
+        model.addAttribute("pcs", servicePc.getPcs());
+        return "showPcs";
+    }
+
+    @GetMapping("/{pcId}")
+    public String showPc(Model model, @PathVariable long pcId){
+        Pc pc = servicePc.getPc(pcId);
+        if (pc==null){
+            return "error";
+        }
+        model.addAttribute("pc", pc);
+        return "showPc";
+    }
+
+    @GetMapping("/{pcId}/updatedPc")
+    public String updatedPc(Model model, @PathVariable long pcId) {
+        model.addAttribute("pcId", pcId);
+        return "updatePc";
+    }
+
+    @GetMapping("/updatePc")
+    public String updatePc(Model model,@RequestParam long pcId, @RequestParam String ram, @RequestParam String graphicCard, @RequestParam String cpu){
+        Pc aux = new Pc(ram, graphicCard, cpu);
+        servicePc.updatePc(pcId, aux);
+        return "updatedPc";
+
+    }
+
+    @GetMapping ("/deletePc/{pcId}")
+    public String deletePc(Model model, @PathVariable long pcId){
+        model.addAttribute("pcId", pcId);
+        servicePc.deletePc(pcId);
+        return "/deletePc";
+    }
 }
--- src/main/java/com/dws/practicaweb/PcService.java
+++ src/main/java/com/dws/practicaweb/PcService.java
@@ -1,4 +1,38 @@
 package com.dws.practicaweb;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
 public class PcService {
+    private Map<Long, Pc> pcs = new ConcurrentHashMap<>();
+    private AtomicLong lastid=new AtomicLong();
+
+    public void addPc(Pc pc){
+        long pcId=lastid.incrementAndGet();
+        pc.setPcId(pcId);
+        pcs.put(pcId, pc);
+    }
+
+    public Collection<Pc> getPcs(){
+        return pcs.values();
+    }
+
+    public Pc getPc(long pcId){
+        return pcs.get(pcId);
+    }
+
+    public void deletePc(long pcId){
+        pcs.remove(pcId);
+    }
+
+    public void updatePc(long pcId, Pc pcMod) {
+        pcMod.setPcId(pcId);
+        pcs.put(pcId, pcMod);
+    }
+
 }
